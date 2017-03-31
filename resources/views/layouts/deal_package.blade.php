f1f870b0620b109ad3be5e9da7e1f53771125b29
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro.local>
2017-03-31 22:42:00 +0700

    ddulichbien

--- resources/views/layouts/deal_package.blade.php
+++ resources/views/layouts/deal_package.blade.php
@@ -6,24 +6,26 @@
       </h2>
       <div class="deals-gallery">
         <div class="owl-carousel owl-theme">
+          <div class="item">
+            <img srcset="./upload/bo-bien.jpg" alt="Khám phá bờ biển Hà Tĩnh" style="padding: 0 5px;">
+            <h2 class="tile-heading" style="text-align:center">
+              <a href="#" title="Khám phá bờ biển Hà Tĩnh">Khám phá bờ biển</a>
+            </h2>
+          </div>
+          <div class="item">
+            <img srcset="./upload/nghi-duong.jpg" alt="Hệ thống khách sạn, resort" style="padding: 0 5px;">
+            <h2 class="tile-heading" style="text-align:center">
+              <a href="#" title="Hệ thống khách sạn, resorth">Hệ thống khách sạn, resort, golf</a>
+            </h2>
+          </div>
             <div class="item">
-              <img srcset="./upload/dua-cho-xuan-thanh.jpg" alt="Trung tâm đua chó Xuân Thành" style="padding: 0 5px;">
+              <img srcset="./upload/dua-cho-xuan-thanh.jpg" alt="Vui chơi, Giải trí" style="padding: 0 5px;">
               <h2 class="tile-heading" style="text-align:center">
-                <a href="#" title="Trung tâm đua chó Xuân Thành">Trung tâm đua chó Xuân Thành</a>
+                <a href="#" title="Vui chơi, Giải trí">Vui chơi, Giải trí</a>
               </h2>
             </div>
-            <div class="item">
-              <img srcset="./upload/nghi-duong.jpg" alt="Hệ thống khách sạn, resort" style="padding: 0 5px;">
-              <h2 class="tile-heading" style="text-align:center">
-                <a href="#" title="Hệ thống khách sạn, resorth">Hệ thống khách sạn, resort, golf</a>
-              </h2>
-            </div>
-            <div class="item">
-              <img srcset="./upload/bo-bien.jpg" alt="Khám phá bờ biển Hà Tĩnh" style="padding: 0 5px;">
-              <h2 class="tile-heading" style="text-align:center">
-                <a href="#" title="Khám phá bờ biển Hà Tĩnh">Khám phá bờ biển</a>
-              </h2>
-            </div>
+
+
         </div>
     </div>
   </div>
--- resources/views/layouts/social_media.blade.php
+++ resources/views/layouts/social_media.blade.php
@@ -9,7 +9,8 @@
 <div class="field field--name-body field--type-text-with-summary field--label-hidden field--item"><div class="child-footer col-sm-12 col-md-6">
 <h3>Theo dõi Du lịch biển Hà Tĩnh</h3>
 
-<ul class="social-networks"><li><a class="icon icon-only fa fa-facebook ga_URL_www-facebook-com_lead_web" href="https://www.facebook.com/" rel="external nofollow" target="_blank">Facebook</a></li>
+<ul class="social-networks">
+<li><a class="icon icon-only fa fa-facebook ga_URL_www-facebook-com_lead_web" href="https://www.facebook.com/dulichbienhatinh" rel="external nofollow" target="_blank">Facebook</a></li>
 <li><a class="icon icon-only fa fa-twitter ga_URL_twitter-com_lead_web" href="https://twitter.com/" rel="external nofollow" target="_blank">Twitter</a></li>
 <li><a class="icon icon-only fa fa-google-plus ga_URL_plus-google-com_lead_web" href="https://plus.google.com/" rel="external nofollow" target="_blank">Google+</a></li>
 <li><a class="icon icon-only fa fa-youtube ga_URL_www-youtube-com_lead_web" href="https://www.youtube.com/" rel="external nofollow" target="_blank">YouTube</a></li>
